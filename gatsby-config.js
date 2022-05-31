4a81c7625675beccc2927721deeae1085cf97b51
Allen Eben <allen.eben@fjorgedigital.com>
2022-05-31 09:35:07 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `Fjorge Contentful Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "digIxyanwWcFv_zGEAFJCLffYHSMmgtNhsBgTCKSIaQ",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};

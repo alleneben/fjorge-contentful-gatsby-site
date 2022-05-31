f6370346c6eed64ff299c88b743153a22a05c922
Allen Eben <allen.eben@fjorgedigital.com>
2022-05-31 11:35:27 +0000

    :Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config()
+
 module.exports = {
   siteMetadata: {
     title: `Fjorge Contentful Gatsby Site`,
@@ -6,15 +9,13 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "digIxyanwWcFv_zGEAFJCLffYHSMmgtNhsBgTCKSIaQ",
-      "spaceId": ""
+      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+      "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
-  }, "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  }, "gatsby-plugin-sass",  
+  "gatsby-plugin-image", 
+  "gatsby-plugin-react-helmet", 
+  "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
